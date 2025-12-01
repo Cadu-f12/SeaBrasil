32b5300f2eebe630e46798d1e9968ab05baa32bb
CarlosLinux <cadudailha68@gmail.com>
2025-12-01 09:45:51 -0300

    feat: Implementar método de obter vendedor por id

--- src/main/java/dao/VendedorDAO.java
+++ src/main/java/dao/VendedorDAO.java
@@ -1,10 +1,12 @@
 package dao;
 
 import model.vendedor.Vendedor;
+import model.vendedor.VendedorInfo;
 import util.Conexao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class VendedorDAO {
@@ -25,4 +27,33 @@ public class VendedorDAO {
             throw new RuntimeException("Ocorreu um erro inesperado durante a execução: " + e);
         }
     }
+
+    public Vendedor obterVendedorPorId(int id) {
+        String sql = "SELECT * FROM vendedor WHERE id = ?";
+        VendedorInfo vendedorInfo = null;
+        Vendedor vendedor = null;
+
+
+        try (Connection conn = Conexao.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                vendedorInfo = new VendedorInfo(rs.getString("nome"), rs.getString("telefone"));
+                vendedor = new Vendedor(rs.getInt("id"), vendedorInfo);
+            }
+
+            return vendedor;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void listarVendedores() {}
+
+    public void atualizarVendedor() {}
+
+    public void deletarVendedor() {}
 }
