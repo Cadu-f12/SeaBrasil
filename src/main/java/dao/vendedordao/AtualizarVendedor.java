a1c919099b3fab6d89e34a2d8681eff9341f472a
CarlosLinux <cadudailha68@gmail.com>
2025-12-08 19:04:21 -0300

    feat: Criar método de atualização de vendedor no vendedorDAO

--- src/main/java/dao/vendedordao/AtualizarVendedor.java
+++ src/main/java/dao/vendedordao/AtualizarVendedor.java
@@ -1,8 +1,26 @@
 package dao.vendedordao;
 
 import model.vendedor.Vendedor;
+import util.Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class AtualizarVendedor {
 
-    public void atualizarVendedor() {}
+    public void atualizarVendedor(Vendedor novoVendedor) {
+        String sql = "UPDATE vendedor SET id = ?, nome = ?, telefone = ? WHERE id = ?";
+
+        try (Connection conn = Conexao.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setInt(1, novoVendedor.captureVendedorId());
+            pstmt.setString(2, novoVendedor.captureNome());
+            pstmt.setString(3, novoVendedor.captureTelefone());
+            pstmt.setInt(4, novoVendedor.captureVendedorId());
+
+            pstmt.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
