c4f31010119858e6323f7b7c06540086d1f11990
CarlosLinux <cadudailha68@gmail.com>
2025-12-16 10:22:57 -0300

    feat: Criar métodos de pesquisa a classe vendaDAO
    Observação, percebi que dá para abstrair as classes CRUD colocando todas em um
    package separado e adicionando uma interface onde irá ser acessado de acordo com a
    implementação

---
+++ src/main/java/dao/vendadao/Pesquisas.java
@@ -0,0 +1,83 @@
+package dao.vendadao;
+
+import dao.produtodao.ProdutoDAO;
+import dao.vendedordao.VendedorDAO;
+import model.Id;
+import model.produto.Produto;
+import model.venda.*;
+import model.vendedor.Vendedor;
+import util.Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Pesquisas {
+
+    public Venda lerId(Venda vendaApenasComId) {
+        String sql = "SELECT * FROM produto WHERE id = ?";
+        Venda venda = null;
+
+        try (Connection conn = Conexao.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, vendaApenasComId.getVendaId());
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                Id idVenda = new Id(rs.getInt("id"));
+                PagamentoData pagamentoData = new PagamentoData(Pagamento.valueOf(rs.getString("formadepagamento")), rs.getDate("data").toLocalDate());
+                Quantidade quantidade = new Quantidade(rs.getInt("quantidade"));
+                ResponsavelPagamento responsavelPagamento = new ResponsavelPagamento(pagamentoData, carregarVendedor(rs.getInt("idvendedor")));
+                ItemVenda itemVenda = new ItemVenda(carregarProduto(rs.getInt("idproduto")), quantidade);
+                VendaInfo vendaInfo = new VendaInfo(itemVenda, responsavelPagamento);
+                venda = new Venda(idVenda, vendaInfo);
+            }
+
+            rs.close();
+            return venda;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<Venda> listar() {
+        String sql = "SELECT * FROM venda";
+        ArrayList<Venda> vendas = new ArrayList<>();
+
+        try (Connection conn = Conexao.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Id idVenda = new Id(rs.getInt("id"));
+                Quantidade quantidade = new Quantidade(rs.getInt("quantidade"));
+                ItemVenda itemVenda = new ItemVenda(carregarProduto(rs.getInt("idproduto")), quantidade);
+                PagamentoData pagamentoData = new PagamentoData(Pagamento.valueOf(rs.getString("formadepagamento")), rs.getDate("data").toLocalDate());
+                ResponsavelPagamento responsavelPagamento = new ResponsavelPagamento(pagamentoData, carregarVendedor(rs.getInt("idvendedor")));
+                VendaInfo vendaInfo = new VendaInfo(itemVenda, responsavelPagamento);
+                Venda venda = new Venda(idVenda, vendaInfo);
+                vendas.add(venda);
+            }
+
+            return vendas;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Vendedor carregarVendedor(int id) {
+        VendedorDAO vendedorDAO = new VendedorDAO();
+        Vendedor vendedor = new Vendedor(new Id(id), null);
+        return vendedorDAO.pesquisarId(vendedor);
+    }
+
+    private Produto carregarProduto(int id) {
+        ProdutoDAO produtoDAO = new ProdutoDAO();
+        Produto produto = new Produto(new Id(id), null);
+        return produtoDAO.pesquisarId(produto);
+    }
+
+
+
+}
--- src/main/java/dao/vendadao/VendaDAO.java
+++ src/main/java/dao/vendadao/VendaDAO.java
@@ -3,19 +3,36 @@ package dao.vendadao;
 import dao.ValidacaoId;
 import model.venda.Venda;
 
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class VendaDAO {
     ValidacaoId validacaoId;
     Criacoes criacoes;
+    Pesquisas pesquisas;
 
     public VendaDAO() {
-        validacaoId = new ValidacaoId();
+        this.validacaoId = new ValidacaoId();
         this.criacoes = new Criacoes();
+        this.pesquisas = new Pesquisas();
+    }
+
+    /* Métodos de pesquisa */
+    public Venda pesquisarId(Venda vendaApenasComId) {
+        boolean b = validacaoId.existeIdProduto(vendaApenasComId.getProdutoId());
+        if (!b) {
+            System.err.println("Exceção disparada de: VendaDAO.pesquisarId");
+            throw new NoSuchElementException("id_venda inválido: não existente no sistema");
+        }
+
+        return pesquisas.lerId(vendaApenasComId);
+    }
+    public ArrayList<Venda> listar() {
+        return pesquisas.listar();
     }
 
     /* Métodos de criação */
-    public void criarComId(Venda venda) {
+    public void resgistrarId(Venda venda) {
         boolean b = validacaoId.existeIdProduto(venda.getVendaId());
         if (b) {
             System.err.println("Exceção disparada de: VendaDAO.criarComId");
@@ -24,7 +41,7 @@ public class VendaDAO {
 
         criacoes.addVendaComId(venda);
     }
-    public void criarComNome(Venda venda) {
+    public void registrarNome(Venda venda) {
 
         criacoes.addVendaComNome(venda);
     }
